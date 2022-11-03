9074b23e049b3d3479f389a91af0a33c9ce06f4e
najat <najatt.ismail@gmail.com>
2022-11-03 09:12:15 +0200

    footer design

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -4,26 +4,26 @@ import {AiOutlineInstagram,AiFillYoutube,AiFillLinkedin} from 'react-icons/ai'
 import {HiOutlineMailOpen,HiLocationMarker} from 'react-icons/hi';
 function Footer() {
     return ( 
-    <footer className="p-4 bg-my-beige-500 sm:p-6">
-    <div className="md:flex md:justify-between">
-        <div className="md:mb-0">
-        <Image src="/logo.png" width="200" height="200" alt="Food Options Logo" />
+    <footer className="p-4 bg-my-beige-500 sm:py-6 sm:px-12">
+    <div className="md:flex md:justify-between mb-6">
+        <div className="p-4">
+            <Image src="/logo.png" width="200" height="200" alt="Food Options Logo" />
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a href="#" className="text-gray-500 hover:text-gray-900">
-                <AiOutlineInstagram color="white"fontSize="1.2em" />
-                <span className="sr-only">Instagram page</span>
-            </a>
-            <a href="#" className="text-gray-500 hover:text-gray-90">
-                <AiFillYoutube color="white"fontSize="1.2em" />
-                <span className="sr-only">Youtube page</span>
-            </a>
-            <a href="#" className="text-gray-500 hover:text-gray-90">
-                <AiFillLinkedin color="white"fontSize="1.2em" />
-                <span className="sr-only">Linkedin page</span>
-            </a>
+                <a href="#" className="text-gray-500 hover:text-gray-900">
+                    <AiOutlineInstagram color="white"fontSize="1.2em" />
+                    <span className="sr-only">Instagram page</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-gray-90">
+                    <AiFillYoutube color="white"fontSize="1.2em" />
+                    <span className="sr-only">Youtube page</span>
+                </a>
+                <a href="#" className="text-gray-500 hover:text-gray-90">
+                    <AiFillLinkedin color="white"fontSize="1.2em" />
+                    <span className="sr-only">Linkedin page</span>
+                </a>
+           </div>
         </div>
-        </div>
-        <div className="grid grid-cols-2 sm:gap-6 sm:w-8/12 sm:grid-cols-3">
+        <div className="grid grid-cols-2 sm:gap-10 sm:w-8/12 sm:grid-cols-3">
             <div>
                 <ul className="text-gray-600">
                     <li class="text-center text-white bg-my-teal-500 hover:bg-my-teal-700 font-bold py-2 px-4 w-full">
@@ -51,9 +51,9 @@ function Footer() {
             </div>
             <div>
                 <h2 className="mb-6 text-sm font-semibold text-gray-900">Subscribe to our newsletter</h2>
-                <ul className="text-gray-600">
+                <ul className="text-gray-600 h-full">
                     <li>
-                        <input className="mb-4 w-full" type="text" id='newsletterreg' name="wdform_2_element7" value="" title="Email" placeholder="Email" onchange="wd_check_email('2', '7', 'This is not a valid email address.')"/>
+                        <input className="mb-6 w-full" type="text" id='newsletterreg' name="wdform_2_element7" value="" title="Email" placeholder="Email" onchange="wd_check_email('2', '7', 'This is not a valid email address.')"/>
                    </li>
                    <li class="text-center text-white bg-my-teal-500 hover:bg-my-teal-700 font-bold py-2 px-4">
                         <button type="button" onClick=''><span class="fm-submit-loading spinner fm-ico-spinner"></span>Subscribe now</button>
@@ -62,7 +62,6 @@ function Footer() {
             </div>
         </div>
     </div>
-    <br/>
     <div className="text-center text-white">
         <span>© 2022<a href="https://flowbite.com/">Flowbite™</a>
         . All Rights Reserved.
